bba5e1527ae27c65cdb74d89a99e294dc5d39421
victor840 <victor.ugoanusi@gmail.com>
2021-01-04 20:59:05 -0500

    Passed all tests

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,5 +1,7 @@
 package com.codedifferently;
 
+import java.util.Arrays;
+
 public class Solution {
 
     /**
@@ -14,7 +16,41 @@ public class Solution {
      */
     public Boolean stringScramble(String str1, String str2) {
         // code goes here
-        return null;
+
+        // intialize string variable scramble and set it to an empty string
+        // intialize boolean variable scramState and set it to false;
+        // intialize char array called arry1
+        // intialize another char array called arry2
+        // set str1 to char array arry1 using the .toCharArray() method
+        // set str2 to char array arry2 using the .toCharArray() method
+        // sort both arrays with  Arrays.sort() method
+        // write a for loop to traverse arry1
+        // write another for loop inside arry1 to traverse arry2
+        // if value at arry1[i] is equal to value at arry[j] set scramState to true;
+        // else set scramState to false;
+        // convert scramState to string with the Boolean.toString(scramState) method
+        // set the resulting string from the instruction above to the scramble variable
+        // return scramble variable
+
+        String scramble = "";
+        boolean scramState = false;
+        char[] arry1 = str1.toCharArray();
+        char[] arry2 = str2.toCharArray();
+        Arrays.sort(arry1);
+        Arrays.sort(arry2);
+
+        for (int i = 0; i < arry1.length; i++) {
+            for (int j = 0; j < arry2.length; j++) {
+                if (arry1[i] == (arry2[j])) {
+                    scramState = true;
+                }
+                else{
+                    return scramState;
+                }
+            }
+        }
+       // scramble = Boolean.toString(scramState);
+        return scramState;
     }
 }
 
